7e2327c9dd1221c1bb6b4e0400e5819f5c17a4ce
Guillaume Robin <guillaume.robin@appi-conseil.com>
2019-01-25 21:55:55 +0100

    Added: SpawnController

---
+++ Assets/LightController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LightController : MonoBehaviour
+{
+    // Public variables
+    public UnityEvent<float> _event;
+
+    // Private variables
+    private CircleCollider2D circleCollider = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.circleCollider = this.GetComponent<CircleCollider2D>();
+        if (this.circleCollider == null)
+        {
+            Debug.LogError("No CircleCollider2D found.", this);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _event.Invoke(this.circleCollider.radius);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/_Project/Scripts/World/SpawnController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+
+    // Public variables
+    public GameObject spriteToSpawn;
+    public CircleCollider2D spawnArea;
+    public uint numberOfSprites = 10;
+
+    // Private variables
+    private uint currentNumOfSprites = 0;
+
+    // Spawns numberOfSprites spriteToSpawn game objects
+    void SpawnObjects()
+    {
+        for (uint i = currentNumOfSprites; i < numberOfSprites; i++)
+        {
+            Instantiate(spriteToSpawn, Random.insideUnitCircle * spawnArea.radius, Quaternion.identity, this.transform);
+            currentNumOfSprites++;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnObjects();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
