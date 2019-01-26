e35f7a4e4c1992e1dcb7fb47259f00ddcc9a37a5
LRP500 <lrp.roy@gmail.com>
2019-01-26 18:24:33 +0100

    fix

--- Assets/_Project/Scripts/PlayerController.cs
+++ Assets/_Project/Scripts/PlayerController.cs
@@ -6,11 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D body;
     public float thrust;
-    
+
     private void Update()
     {
-       if (Input.GetKeyDown("left")) body.AddForce(-Vector3.right * thrust);
-       if (Input.GetKeyDown("right")) body.AddForce(Vector3.right * thrust);
-       if (Input.GetKeyDown("up")) body.AddForce(Vector3.up * thrust);
-       if (Input.GetKeyDown("down")) body.AddForce(-Vector3.up * thrust);
+        if (Input.GetKeyDown("left")) body.AddForce(-Vector3.right * thrust);
+        if (Input.GetKeyDown("right")) body.AddForce(Vector3.right * thrust);
+        if (Input.GetKeyDown("up")) body.AddForce(Vector3.up * thrust);
+        if (Input.GetKeyDown("down")) body.AddForce(-Vector3.up * thrust);
     }
+}
