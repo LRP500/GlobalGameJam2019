7dcb8dfc4be12d780ee8ee43069d28f612b57ffb
Wilfried Magne <wilfried.magne@appi-conseil.com>
2019-01-26 18:18:38 +0100

    working

--- Assets/_Project/Scripts/PlayerController.cs
+++ Assets/_Project/Scripts/PlayerController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D body;
     public float thrust;
 
+    public float maxSpeed;
+
     public Collider2D trigger;
+
+  private Rigidbody2D body;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        return ;
+        Vector3 velocity = body.velocity;
+        if (velocity.magnitude > maxSpeed) {
+            body.velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+        }
     }
 
     // Update is called once per frame
@@ -20,29 +32,30 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown("left")) {
             body.AddForce(-Vector3.right * thrust);
+            //body.velocity = Vector3.ClampMagnitude(-Vector3.right, thrust);
         }
         if (Input.GetKeyDown("right")) {
             body.AddForce(Vector3.right * thrust);
+            //body.velocity = Vector3.ClampMagnitude(Vector3.right, thrust);
         }
         if (Input.GetKeyDown("up")) {
             body.AddForce(Vector3.up * thrust);
+            //body.velocity = Vector3.ClampMagnitude(Vector3.up, thrust);
         }
         if (Input.GetKeyDown("down")) {
             body.AddForce(-Vector3.up * thrust);
+            //body.velocity = Vector3.ClampMagnitude(-Vector3.up, thrust);
         }
     }
 
-    /// <summary>
-    /// Sent when another object enters a trigger collider attached to this
-    /// object (2D physics only).
-    /// </summary>
-    /// <param name="other">The other Collider2D involved in this collision.</param>
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerExit2D(Collider2D other)
     {
         if (other.gameObject.tag == "Void") {
-            Debug.Log(gameObject.GetComponent<Rigidbody2D>().velocity);
-            Vector3 velocity = gameObject.GetComponent<Rigidbody2D>().velocity;
-            gameObject.GetComponent<Rigidbody2D>().velocity = - velocity;
+            Vector3 velocity = gameObject.GetComponent<Rigidbody2D>().velocity.normalized;
+            //transform.position = new Vector3(transform.position.x, transform.position.y, -7);
+            gameObject.GetComponent<Rigidbody2D>().velocity = -velocity * 2;
+            //Vector3 point = other.gameObject.GetComponent<Collider2D>().Close(transform.position);
+            //Debug.Log(point);
         }
     }
 }
