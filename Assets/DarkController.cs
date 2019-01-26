3e4a50b65f2d408660da9381c2a0a09077289535
Wilfried Magne <wilfried.magne@appi-conseil.com>
2019-01-26 18:44:52 +0100

    done

---
+++ Assets/DarkController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float _shrinkSpeed;
+    void Start()
+    {
+        
+    }
+
+  void FixedUpdate()
+  {
+    Vector3 scale = transform.localScale;
+    scale.x -= _shrinkSpeed * Time.deltaTime;
+    scale.y -= _shrinkSpeed * Time.deltaTime;
+    transform.localScale = scale;
+  }
+}
