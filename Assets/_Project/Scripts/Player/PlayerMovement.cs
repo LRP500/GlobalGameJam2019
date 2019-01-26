8683c0aa9260fde4d3844bfa85adb4143c3195be
Guillaume Robin <guillaume.robin@appi-conseil.com>
2019-01-26 03:23:52 +0100

    Fixed: player middle sickness

--- Assets/_Project/Scripts/Player/PlayerMovement.cs
+++ Assets/_Project/Scripts/Player/PlayerMovement.cs
@@ -7,12 +7,12 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D rigidbody;
     private bool repulsionSetted = true;
+    private bool startMove = true;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
-        Vector3 speed = new Vector3(1, 1, 1);
     }
 
     public void EnableRepulsion()
@@ -28,16 +28,30 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 gravity = new Vector3(-transform.position.x, -transform.position.y, 0);
-        gravity.Normalize();
-        gravity *= _gravity;
-        Vector3 translation = new Vector3(Input.GetAxis("Horizontal") * _speed, Input.GetAxis("Vertical") * _speed, 0);
-        Vector3 speed = (gravity + translation);
-
-        if (repulsionSetted)
+        if (System.Math.Abs(Input.GetAxis("Horizontal")) > 0.01 || System.Math.Abs(Input.GetAxis("Vertical")) > 0.01)
         {
-            speed = gravity;
+            startMove = true;
         }
-        rigidbody.velocity = speed;
+        else if (System.Math.Abs(transform.position.x) < 5 && System.Math.Abs(transform.position.y) < 5)
+        {
+            rigidbody.velocity = Vector3.zero;
+            transform.position = new Vector3(0, 0, transform.position.z);
+            startMove = false;
+        }
+
+        if (startMove)
+        {
+            Vector3 gravity = new Vector3(-transform.position.x, -transform.position.y, 0);
+            gravity.Normalize();
+            gravity *= _gravity;
+            Vector3 translation = new Vector3(Input.GetAxis("Horizontal") * _speed, Input.GetAxis("Vertical") * _speed, 0);
+            Vector3 speed = (gravity + translation);
+
+            if (repulsionSetted)
+            {
+                speed = gravity;
+            }
+            rigidbody.velocity = speed;
+        }
     }
 }
