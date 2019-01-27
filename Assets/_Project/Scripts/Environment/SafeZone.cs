2c698b9974a149fdeb2c0451c519158765f98503
LRP500 <lrp.roy@gmail.com>
2019-01-27 03:46:53 +0100

    minor fixes

--- Assets/_Project/Scripts/Environment/SafeZone.cs
+++ Assets/_Project/Scripts/Environment/SafeZone.cs
@@ -7,6 +7,8 @@ namespace GlobalGameJam2019
 		#region Private Variables
 
 	    [SerializeField] private Void _void;
+        
+        public Color _color = new Color(0.08627451f, 0.509804f, 0.9843138f, 1f);
 	    
 		#endregion Private Variables
 	
@@ -15,13 +17,15 @@ namespace GlobalGameJam2019
 	    private void OnTriggerEnter2D(Collider2D other)
 	    {
             var sprite = GetComponent<SpriteRenderer>();
-            sprite.color = new Color(0.08627451f, 0.509804f, 0.9843138f, 0.09411765f);
-        }
+		    Color newColor = _color;
+		    newColor.a *= 2;
+	        sprite.color = newColor;
+	    }
 
         private void OnTriggerExit2D(Collider2D other)
         {
             var sprite = GetComponent<SpriteRenderer>();
-            sprite.color = new Color(0.08627451f, 0.509804f, 0.9843138f, 0.03921569f);
+            sprite.color = _color;
         }
 
         private void OnTriggerStay2D(Collider2D other)
