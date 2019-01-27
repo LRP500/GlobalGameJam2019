2a8b29837c8a45f0501dda759aec4f89942235f7
LRP500 <lrp.roy@gmail.com>
2019-01-27 13:25:14 +0100

    persistent high score

--- Assets/_Project/Scripts/GameManager.cs
+++ Assets/_Project/Scripts/GameManager.cs
@@ -1,15 +1,24 @@
-﻿using UnityEngine;
-using UnityEngine.Events;
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using Variables;
 
 namespace GlobalGameJam2019
 {
     public class GameManager : MonoBehaviour
     {
+        private const string _saveFileName = "HighScore.secure";
+
         #region Private Variables
 
         [SerializeField] private Void _void;
         [SerializeField] private GameOverScreen _gameOverScreen;
         [SerializeField] private Transform _playerTransform;
+        [SerializeField] private IntVariable _playerScore;
+        
+        private string _saveDataPath;
+        private SaveData _save;
         
         #endregion Private Variables
 
@@ -17,6 +26,7 @@ namespace GlobalGameJam2019
 
         private void Awake()
         {
+            LoadHighScore();
             Time.timeScale = 1;
         }
 
@@ -41,10 +51,54 @@ namespace GlobalGameJam2019
 
         public void GameOver()
         {
-            _gameOverScreen.Display();
+            if (_playerScore.Value > _save.HighScore)
+            {
+                _save.HighScore = _playerScore.Value;
+                RecordHighScore();
+            }
+
+            _gameOverScreen.Display(_save.HighScore);
             Time.timeScale = 0;
         }
 
+        public void RecordHighScore()
+        {
+            if (Directory.Exists(_saveDataPath) == false)
+                Directory.CreateDirectory(_saveDataPath);
+
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Create(_saveDataPath + _saveFileName);
+            bf.Serialize(file, _save);
+            file.Close();
+        }
+
+        public void LoadHighScore()
+        {
+            _saveDataPath = Application.dataPath + "/Save/";
+            if (File.Exists(_saveDataPath + _saveFileName))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                FileStream file = File.Open(_saveDataPath + _saveFileName, FileMode.Open);
+                _save = (SaveData)bf.Deserialize(file);
+                file.Close();
+                
+                Debug.Log("Highscore saved");
+            }
+            else
+            {
+                Debug.LogWarning("No existing save data found. Creating save data...");
+                _save = new SaveData();
+            }
+        }
+        
         #endregion Public Methods
+    }
+
+    [Serializable]
+    public class SaveData
+    {
+        public int HighScore;
+
+        public SaveData() { HighScore = 0; }
     }
 }
--- Assets/_Project/Scripts/UI/GameOverScreen.cs
+++ Assets/_Project/Scripts/UI/GameOverScreen.cs
@@ -6,8 +6,9 @@ public class GameOverScreen : MonoBehaviour
 {
     #region Private Variables
 
-    [SerializeField] private TextMeshProUGUI _scoreText;
-    [SerializeField] private IntVariable _scoreData;
+    [SerializeField] private TextMeshProUGUI _playerScoreText;
+    [SerializeField] private TextMeshProUGUI _highScoreText;
+    [SerializeField] private IntVariable _playerScore;
     [SerializeField] private Canvas _canvas;
 
     #endregion Private Variables
@@ -26,12 +27,14 @@ public class GameOverScreen : MonoBehaviour
     #endregion Private Methods
 
     #region Public Methods
+    
+    public void Display(int highscore)
+    {
+        _highScoreText.text = $"Best: {highscore:n0}".Replace(",", " ");
+        _playerScoreText.text = $"Score: {_playerScore.Value:n0}".Replace(",", " ");
+        _canvas.enabled = true;
+    }
 
     #endregion Public Methods
 
-    public void Display()
-    {
-        _scoreText.text = $"Score: {_scoreData.Value}";
-        _canvas.enabled = true;
-    }
 }
